a22ee420a61bfa7fe6aca0a9f99fb64cbb9f87ce
lddorse <lddorse@gmail.com>
2026-05-26 10:57:37 -0500

    fix scroll snap container

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,24 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/work" element={<WorkPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
+      <div id="scroll-root" style={{
+        height: 'calc(100dvh - 64px)',
+        overflowY: 'scroll',
+        scrollSnapType: 'y mandatory',
+        WebkitOverflowScrolling: 'touch',
+        position: 'fixed',
+        top: 64,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/work" element={<WorkPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
